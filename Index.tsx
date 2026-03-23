2da44a4679bab4c6a135e8124cc153fe04acc4ce
anibart3-oss <anibart3@gmail.com>
2026-03-23 17:41:50 +0400

    Create Index.tsx for portfolio homepage component

---
+++ Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioHomepage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is the homepage of my portfolio showcasing my projects and skills.</p>
+        </div>
+    );
+};
+
+export default PortfolioHomepage;
